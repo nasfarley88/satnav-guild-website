26be1bacaeb40ffe799d8baf675b2ac82300ade4
Nathanael Farley <nasfarley88@eplt02.ph.bham.ac.uk>
2013-11-10 21:47:54 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<head>
+<title>SATNAV Magazine</title>
+<style>
+.fullscreenDiv {
+    background-color: #e8e8e8;
+    width: 100%;
+    height: auto;
+    bottom: 0px;
+    top: 0px;
+    left: 0;
+    position: absolute;
+}
+
+.center {
+    position: absolute;
+    width: 300px;
+    height: 50px;
+    top: 50%;
+    left: 50%;
+    margin-left: -150px; /* margin is -0.5 * dimension */
+    margin-top: -25px; 
+}​
+</style>
+</head>
+
+<body>
+<div class='fullscreenDiv'>
+    <div class="center"><center>SATNAV Website
+coming soon...<br />
+<a href="https://www.facebook.com/SatnavMagazine">Facebook</a> <a href="https://twitter.com/satnavmag">Twitter</a> <a href="https://plus.google.com/113430861538777394105" rel="publisher">Google+</a></center>
+</div>
+</div>
+</body>
